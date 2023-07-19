55de6125b0a2bd0231878b20bc7e1fc67b5d6775
sssadboy <Fifashf@yandex.ru>
2023-07-20 01:40:50 +0300

    Добавил запуск файла с параметром

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,9 +1,17 @@
+import argparse
 import requests
 from save_tools import save_picture
 
 
 def main():
-    id = '5eb87d42ffd86e000604b384'
+    parser = argparse.ArgumentParser(description='Данный файл скачивает фотографии с последнего запуска')
+    parser.add_argument('--id', 
+                        type=str,
+                        default='5eb87d42ffd86e000604b384', 
+                        help='Введите id последнего запуска'
+                        )
+    args = parser.parse_args()
+    id = args.id
     url = 'https://api.spacexdata.com/v5/launches/{}'.format(id)
     response = requests.get(url)
     response.raise_for_status()
