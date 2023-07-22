96543a4dd2ccfc5bcbd842b9161ee544e7dc682b
sssadboy <Fifashf@yandex.ru>
2023-07-22 15:02:27 +0300

    publish photo to Telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -1,9 +1,13 @@
 import os
 import telegram
 
-tg_token = os.getenv("TG_TOKEN")
-bot = telegram.Bot(token="6137754993:AAGRGpMcgxFMmt27AYjIi2vqa7SvuN7mRQk")
+def main():
+    tg_token = os.getenv("TG_TOKEN")
+    bot = telegram.Bot(token="6137754993:AAGRGpMcgxFMmt27AYjIi2vqa7SvuN7mRQk")
+    bot.send_message(chat_id='@nasa_apishka', text="I'm sorry Dave I'm afraid I can't do that.")
+    bot.send_document(chat_id='@nasa_apishka', document=open('images/epic_1.png', 'rb'))
+    print(bot.get_me())
 
-bot.send_message(chat_id='@nasa_apishka', text="I'm sorry Dave I'm afraid I can't do that.")
 
-print(bot.get_me())
+if __name__ == '__main__':
+    main()
